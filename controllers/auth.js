cd9fa2e2ad5ddc1d39a7512acb825088328da51d
Nataly <shimmerbodycream@gmail.com>
2024-06-16 13:23:53 -0500

    trial

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,13 +6,20 @@ const bcrypt = require('bcryptjs');
 
 const register = async (req, res) => {
     const { name, email, password } = req.body;
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+ 
+    const tempUser = {name,email,password:hashedPassword};
+    
+
     if (!name || !email || !password) {
         throw new BadRequestError('Please provide name, email, and/or password');
     }
-    const user = await User.create({...req.body});
+    const user = await User.create({...tempUser});
     res.status(StatusCodes.CREATED).json({ user });
 }
-
+//testing delete
 const login = (req, res) => {
 res.send('login user')
 }
