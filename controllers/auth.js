681a34a874213d1494c1092ea187eb6c75981004
NatalyHelps <contactmenataly@gmail.com>
2024-06-16 16:37:21 -0500

    loginandregister

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,27 +1,30 @@
 const User = require('../models/User.js');
 const { StatusCodes } = require('http-status-codes');
-const { BadRequestError } = require('../errors');
-const bcrypt = require('bcryptjs');
-
+const { BadRequestError, UnauthenticatedError } = require('../errors');
 
 const register = async (req, res) => {
-    const { name, email, password } = req.body;
 
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
- 
-    const tempUser = {name,email,password:hashedPassword};
-    
+    const user = await User.create({ ...req.body });
+    const token = user.createJWT();
 
-    if (!name || !email || !password) {
-        throw new BadRequestError('Please provide name, email, and/or password');
-    }
-    const user = await User.create({...tempUser});
-    res.status(StatusCodes.CREATED).json({ user });
+    res.status(StatusCodes.CREATED).json({ user: {name: user.name}, token });
 }
 
-const login = (req, res) => {
-res.send('login user')
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        throw new BadRequestError('Please provide email and password');
+    }
+
+    const user = await User.findOne({ email });
+    
+    if (!user) {
+        throw new UnauthenticatedError('Please provide proper credentials');
+    }
+
+      const token = user.createJWT();
+
+      res.status(StatusCodes.OK).json({user: {name:user.name} , token });
 }
 
 module.exports = {
--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -19,10 +21,21 @@ const UserSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: [true, 'Please provide name'],
-        minLength: 3,
-        maxLength: 12
+        required: [true, 'Please provide password'],
+        minLength: 3
     }
 })
+
+UserSchema.pre('save', async function () {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+})
+
+UserSchema.methods.createJWT = function() {
+    return jwt.sign({ userID: this._id, name: this.name }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_LIFETIME,
+    });
+}
+
 
 module.exports = mongoose.model('User', UserSchema);
