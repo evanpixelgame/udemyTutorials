013bc455ced0b7d3b80c32e2119b5ec8827b00a9
NatalyHelps <contactmenataly@gmail.com>
2024-06-17 01:27:20 -0500

    added update

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -50,7 +50,6 @@ const createJob = async (req, res) => {
 }
 
 const updateJob = async (req, res) => {
-
     const {
         body: {company, position},
         user: { userId },
@@ -61,16 +60,17 @@ const updateJob = async (req, res) => {
         throw new BadRequestError('Company and position fields cannot be empty');
     }
     
-
-  const job = await Job.findOne({
-    _id: jobId,
-    createdBy: userId,
-  })
+    const job = await Job.findByIdAndUpdate(
+        { _id: jobId, createdBy: userId },
+        req.body,
+        { new: true, runValidators: true }
+    );
+    
   if (!job) {
   throw new NotFoundError(`No job with id ${jobId}`)
-  }
+    }
+    
   res.status(StatusCodes.OK).json({ job })
-    res.send('update job');
 }
 
 const deleteJob = async (req, res) => {
