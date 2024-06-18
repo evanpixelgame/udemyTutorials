9395faf4216c8d04272254316004d9c98744ac4c
NatalyHelps <contactmenataly@gmail.com>
2024-06-17 22:44:26 -0500

    swaggeragain?

--- app.js
+++ app.js
@@ -15,42 +15,41 @@ const swaggerDocument = YAML.load('./swagger.yaml');
 const express = require('express');
 const app = express();
 
-// connect DB
-const connectDB = require('./db/connect.js');
-const authenticateUser = require('./middleware/authentication.js');
-
+const connectDB = require('./db/connect');
+const authenticateUser = require('./middleware/authentication');
 // routers
-const authRouter = require('./routes/auth.js');
-const jobsRouter = require('./routes/jobs.js');
-
+const authRouter = require('./routes/auth');
+const jobsRouter = require('./routes/jobs');
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-app.set('trust proxy', 1); //is this still needed or is this only for heroku???
-app.use(rateLimiter({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-}));
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-// routes
 app.get('/', (req, res) => {
-  res.send(`<h1>Jobs API</h1><a href="/api-docs">Documentation</a>`);
-})
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
 
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(YAML.load('./swagger.yaml')));
 
+// routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
